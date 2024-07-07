130bbe65d555fbc5e9607547e1ee735eb4b69691
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-07-07 11:40:42 +0530

    Strategy Design Pattern with Factory Method Implementation

---
+++ src/main/java/qa/design/strategy/PaymentModeFactory.java
@@ -0,0 +1,23 @@
+package qa.design.strategy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class PaymentModeFactory {
+
+    private static final Supplier<PaymentMode> CC = CreditCard::new;
+    private static final Supplier<PaymentMode> NB = NetBanking::new;
+
+    private static final Map<String, Supplier<PaymentMode>> map = new HashMap<>();
+
+    static {
+        map.put("CC", CC);
+        map.put("NB", NB);
+    }
+
+    public static PaymentMode getInstance(String payMode){
+        return map.get(payMode).get();
+    }
+
+}
---
+++ src/test/java/qa/design/strategy/PaymentWindowUsingFactoryTest.java
@@ -0,0 +1,51 @@
+package qa.design.strategy;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.collections.Maps;
+import qa.design.test.BaseTest;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class PaymentWindowUsingFactoryTest extends BaseTest {
+
+    private PaymentWindow paymentWindow;
+
+    @BeforeTest
+    public void setPaymentWindow() {
+        this.paymentWindow = new PaymentWindow(driver);
+    }
+
+    @Test(dataProvider = "fetchData")
+    public void paymentModeTest(String paymentMode, Map<String, String> paymentDetail) {
+        this.paymentWindow.navigateTo();
+        this.paymentWindow.getUserInformation().enterUserDetails("John", "Smith", "john.smith@gmail.com");
+        this.paymentWindow.setPaymentMode(PaymentModeFactory.getInstance(paymentMode));
+        this.paymentWindow.getPaymentMode().enterPaymentDetail(paymentDetail);
+        String orderNumber = this.paymentWindow.getBuyOrder().purchaseOrder();
+        System.out.println("Order Number : " + orderNumber);
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+    }
+
+    @DataProvider
+    public Object[][] fetchData() {
+        Map<String, String> creditCard = Maps.newHashMap();
+        creditCard.put("cardNumber", "987634562312");
+        creditCard.put("cardYear", "2024");
+        creditCard.put("cardPin", "2325");
+
+        Map<String, String> netBanking = Maps.newHashMap();
+        netBanking.put("bank", "WELLS FARGO");
+        netBanking.put("accountNumber", "987634562312");
+        netBanking.put("accountPin", "2325");
+
+        return new Object[][]{
+                {"CC", creditCard},
+                {"NB", netBanking}
+        };
+    }
+
+}
