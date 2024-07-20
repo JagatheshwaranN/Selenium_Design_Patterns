ee57ede6b0b0cfc6f819bb1e25a0960b1a232e62
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-07-20 10:27:08 +0530

    Functional Programming - Supplier Concept with Driver Invocation

---
+++ src/main/java/qa/fp/supplier/DriverFactory.java
@@ -0,0 +1,31 @@
+package qa.fp.supplier;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class DriverFactory {
+
+    // private static final Supplier<WebDriver> chromeDriver = () -> new ChromeDriver();
+    private static final Supplier<WebDriver> chromeDriver = ChromeDriver::new;
+    private static final Supplier<WebDriver> firefoxDriver = FirefoxDriver::new;
+    private static final Supplier<WebDriver> edgeDriver = EdgeDriver::new;
+
+    private static final Map<String, Supplier<WebDriver>> driverMap = new HashMap<>();
+
+    static {
+        driverMap.put("Chrome", chromeDriver);
+        driverMap.put("Firefox", firefoxDriver);
+        driverMap.put("Edge", edgeDriver);
+    }
+
+    public static WebDriver getDriver(String browser){
+        return driverMap.get(browser).get();
+    }
+
+}
---
+++ src/test/java/qa/fp/supplier/DriverTest.java
@@ -0,0 +1,29 @@
+package qa.fp.supplier;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class DriverTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    @Parameters("browser")
+    public void setDriver(@Optional("Chrome") String browser) {
+        this.driver = DriverFactory.getDriver(browser);
+        this.driver.manage().window().maximize();
+    }
+
+    @Test
+    public void seleniumSiteTest() {
+        this.driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(this.driver.getTitle(), "Selenium");
+    }
+
+    @AfterTest
+    public void quitDriver() {
+        this.driver.quit();
+    }
+
+}
